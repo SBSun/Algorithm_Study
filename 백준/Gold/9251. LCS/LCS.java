962e1b195ecaf497c67b18a5f37afefc7a8c3e51
ByungSun Song <46879319+SBSun@users.noreply.github.com>
2023-11-05 23:58:59 +0900

    [Gold V] Title: LCS, Time: 156 ms, Memory: 18444 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str1 = br.readLine();
+		int len1 = str1.length();
+		String str2 = br.readLine();
+		int len2 = str2.length();
+		
+		int[][] dp = new int[len2+1][len1+1];
+		
+		for(int i = 1; i <= len2; i++) {
+			char c = str2.charAt(i-1);
+			for(int j = 1; j <= len1; j++) {
+                // 같으면 LCS 값 1 증가
+				if(c == str1.charAt(j-1)) {
+					dp[i][j] = dp[i-1][j-1] + 1;
+				}else {
+					dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+				}
+			}
+		}
+
+		System.out.println(dp[len2][len1]);
+    }
+}

eae37386083f2ee177238f8f1c5ed3011aa2a680
ByungSun Song <46879319+SBSun@users.noreply.github.com>
2023-11-18 23:53:53 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 152 ms, Memory: 14480 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n+1];
+        int[] dp = new int[n+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 1; i <= n; i++){
+            nums[i] = Integer.parseInt(st.nextToken());   
+            dp[i] = 1;  
+        }
+
+        int min = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                // 이전 원소들 중 가장 작은 dp값 + 1
+                if (nums[i] < nums[j] && dp[i] < dp[j] + 1) 
+                    dp[i] = dp[j] + 1;
+            }
+            min = Math.max(min, dp[i]); 
+        }
+
+        System.out.print(min);
+    }
+}
